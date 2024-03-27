71ae83791bd3a6584fca90acaf237543de2120b5
Anuj Sharma <148482166+anujbhsharma@users.noreply.github.com>
2024-03-27 13:38:27 -0300

    Update views.py

--- account/views.py
+++ account/views.py
@@ -10,6 +10,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 # Create your views here.
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
